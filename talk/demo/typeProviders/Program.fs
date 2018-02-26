c4106735140f30defc430547908daa18eb6a453b
Chester Burbidge <cburbidge@scottlogic.co.uk>
2018-02-26 16:28:11 +0000

    Added type provider demo

---
+++ talk/demo/typeProviders/Program.fs
@@ -0,0 +1,10 @@
+﻿open FSharp.Data
+open System
+
+type Grants = CsvProvider<"https://raw.githubusercontent.com/Devon-County-Council/spending/master/DCCSpendingOver500_201101.csv">
+
+[<EntryPoint>]
+let main argv =
+    use grants = new Grants()
+    
+    0
