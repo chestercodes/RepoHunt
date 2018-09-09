534af5ed2181559c9361a3800d0767d0dfdd1682
Chester Burbidge <chester.burbidge@gmail.com>
2018-09-09 17:18:50 +0100

    Removed some files and renamed others

--- talk/demo/async/Workflows.fsx
+++
@@ -1,7 +0,0 @@
-
-let task1 = async { return 10 + 10 }
-let task2 = async { return 20 + 20 }
-let p = Async.Parallel [ task1; task2 ]
-let result = Async.RunSynchronously p
-
-
--- talk/demo/typeProviders/CreateTables.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists top_10_expenses (
-	supplier_name text not null,
-	total real not null
-);
-
