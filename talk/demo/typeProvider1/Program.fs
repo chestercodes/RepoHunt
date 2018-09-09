8c2693c49e50d4c11887e5303555f0fe658afda2
Chester Burbidge <chester.burbidge@gmail.com>
2018-09-09 17:07:03 +0100

    Added stuff

--- talk/demo/typeProvider1/Program.fs
+++ talk/demo/typeProvider1/Program.fs
@@ -1,20 +1,15 @@
-﻿open System
-//open FSharp.Text.RegexProvider
-
-let [<Literal>] postcodePattern = @"(?<Region>^[A-Z]{1,2})\d{1,2}\s*\d{1,2}[A-Z]{1,2}$"
+﻿
+let [<Literal>] pattern = @"(?<Region>^[A-Z]{1,2})\d{1,2}\s*\d{1,2}[A-Z]{1,2}$"
 
 let parseRegion input = 
     None
     
 [<EntryPoint>]
 let main argv = 
-    
     let input = "AB12 34CD"
-    
+    printfn "try to parse '%s' " input
     let region = parseRegion input
-    
     printfn "%s" (if region.IsSome then region.Value else "failed to parse")
-    
     0 // return an integer exit code
 
     
