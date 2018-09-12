02b76a604c426bc8ca9c5af56395a2e04dde03d2
Chester Burbidge <chester.burbidge@gmail.com>
2018-09-12 14:27:13 +0100

    Added interop code and image

---
+++ talk/code/interop/console/Program.cs
@@ -0,0 +1,19 @@
+﻿using shared.domain;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person(
+                "John", 30,
+                new Address("1 lane", "1 street", "BS11BS"));
+            var john2 = new Person(
+                "John", 30,
+                new Address("1 lane", "1 street", "BS11BS"));
+            System.Console.WriteLine("Johns are equal - " + (john.Equals(john2)));
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ talk/code/interop/shared/Domain.fs
@@ -0,0 +1,4 @@
+﻿namespace shared.domain
+
+type Address = {Line1: string; Line2: string; PostCode: string; }
+type Person = {Name: string; Age: int; Address: Address}
