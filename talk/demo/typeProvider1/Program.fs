0f3f8ce4a3bd7d94f650887826052cd2521eed27
Chester Burbidge <chester.burbidge@gmail.com>
2018-09-16 17:33:55 +0100

    Predemo

--- talk/demo/typeProvider1/Program.fs
+++ talk/demo/typeProvider1/Program.fs
@@ -1,5 +1,4 @@
-﻿
-let [<Literal>] pattern = @"(?<Region>^[A-Z]{1,2})\d{1,2}\s*\d{1,2}[A-Z]{1,2}$"
+﻿let [<Literal>] pattern = @"(?<Region>^[A-Z]{1,2})\d{1,2}\s*\d{1,2}[A-Z]{1,2}$"
 
 let parseRegion input = 
     None
@@ -7,9 +6,11 @@ let parseRegion input =
 [<EntryPoint>]
 let main argv = 
     let input = "AB12 34CD"
-    printfn "try to parse '%s' " input
-    let region = parseRegion input
-    printfn "%s" (if region.IsSome then region.Value else "failed to parse")
+    printfn "try to parse the input %s" input
+    let result = match parseRegion input with 
+                 | Some a ->  sprintf "success! region - %s" a
+                 | None   -> "failed to parse"
+    printfn "%s" result
     0 // return an integer exit code
 
     
